dcfd2a3d9faad4612441b46889b706333b0ab65e
Yoshino-s <yoshino.prog@gmail.com>
2022-06-09 10:31:26 +0800

    feat: port to golang

---
+++ uac/uac_linux.go
@@ -0,0 +1,5 @@
+package uac
+
+func RunMeElevated() {
+	//
+}
---
+++ uac/uac_windows.go
@@ -0,0 +1,44 @@
+package uac
+
+import (
+	"fmt"
+	"golang.org/x/sys/windows"
+	"os"
+	"strings"
+	"syscall"
+)
+
+func RunMeElevated() {
+	if !amAdmin() {
+		runMeElevated()
+	}
+}
+
+func runMeElevated() {
+	verb := "runas"
+	exe, _ := os.Executable()
+	cwd, _ := os.Getwd()
+	args := strings.Join(os.Args[1:], " ")
+
+	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+	exePtr, _ := syscall.UTF16PtrFromString(exe)
+	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+	argPtr, _ := syscall.UTF16PtrFromString(args)
+
+	var showCmd int32 = 1 //SW_NORMAL
+
+	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func amAdmin() bool {
+	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+	if err != nil {
+		fmt.Println("admin no")
+		return false
+	}
+	fmt.Println("admin yes")
+	return true
+}
