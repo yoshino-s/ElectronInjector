d8c96e6334d65584a2b69a7098abe95d35043dbd
Yoshino-s <yoshino.prog@gmail.com>
2022-06-14 08:04:26 +0800

    feat: better inject

--- injector/injector.go
+++ injector/injector.go
@@ -3,18 +3,15 @@ package injector
 import (
 	"bytes"
 	"embed"
-	_ "embed"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
-	"regexp"
-	"strings"
 )
 
 var (
-	inject_new = "function validateString(){};"
-	inject_old = regexp.MustCompile(`function validateString[^}]+}`)
+	inject_new = []byte("electrom")
+	inject_old = []byte("electron")
 )
 
 //go:embed inject
@@ -23,18 +20,17 @@ var injectFs embed.FS
 func Inject(prog string, inject_name string) error {
 	data, err := injectFs.ReadFile(path.Join("inject", inject_name+".js"))
 	if err == nil {
-		return inject(prog, inject_name+".js", data)
+		return inject(prog, data)
 	} else {
-		fmt.Printf("%s\n", err)
 		data, err := ioutil.ReadFile(inject_name)
 		if err != nil {
 			return err
 		}
-		return inject(prog, inject_name, data)
+		return inject(prog, data)
 	}
 }
 
-func inject(prog string, inject_name string, inject_content []byte) error {
+func inject(prog string, inject_content []byte) error {
 	if _, err := os.Stat(prog + ".bak"); err == nil {
 		fmt.Println("Backup file already exists, skipping backup")
 	} else {
@@ -50,26 +46,21 @@ func inject(prog string, inject_name string, inject_content []byte) error {
 		}
 		return err
 	}
-	inject_point := inject_old.Find(prog_content)
-	if inject_point == nil {
+	inject_count := bytes.Count(prog_content, inject_old)
+	if inject_count != 1 {
 		return fmt.Errorf("%s does not contain valid injection point", prog)
 	}
 
-	inject_path := path.Join(path.Dir(prog), "..", "node_modules", path.Base(inject_name))
+	inject_path := path.Join(path.Dir(prog), "..", "node_modules", string(inject_new)+".js")
+	inject_content = []byte(fmt.Sprintf("%s;\nmodule.exports = require(\"electron\");", inject_content))
 	err = ioutil.WriteFile(inject_path, inject_content, 0644)
 	if err != nil {
 		return err
 	}
 
-	inject_payload := fmt.Sprintf("%s;mod.require('%s')", inject_new, path.Base(inject_name))
+	inject_payload := inject_new
 
-	if len(inject_payload) > len(inject_point) {
-		return fmt.Errorf("Inject payload is too long")
-	}
-
-	inject_payload = strings.Repeat(" ", len(inject_point)-len(inject_payload)) + inject_payload
-
-	prog_content = []byte(bytes.Replace(prog_content, inject_point, []byte(inject_payload), 1))
+	prog_content = []byte(bytes.Replace(prog_content, inject_old, inject_payload, 1))
 
 	err = ioutil.WriteFile(prog, prog_content, 0644)
 	if err != nil {
